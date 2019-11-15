ee318ea7bf552f989c74bd9a2ce454b2638899d3
JC Ivancevich <jc.ivancevich@gmail.com>
2019-11-15 15:32:19 -0300

    apply prettier formatting

--- src/Chunk.js
+++ src/Chunk.js
@@ -1,6 +1,6 @@
-import { slice } from "./utils/buffer.js";
-import isFrameHeader from "./utils/isFrameHeader.js";
-import getFrameLength from "./utils/getFrameLength.js";
+import { slice } from './utils/buffer.js';
+import isFrameHeader from './utils/isFrameHeader.js';
+import getFrameLength from './utils/getFrameLength.js';
 
 export default class Chunk {
   constructor({ clip, raw, onready, onerror }) {
@@ -56,7 +56,7 @@ export default class Chunk {
   createSource(timeOffset, callback, errback) {
     if (!this.ready) {
       console.error(
-        "Something went wrong! Chunk was not ready in time for playback"
+        'Something went wrong! Chunk was not ready in time for playback'
       );
       return;
     }
