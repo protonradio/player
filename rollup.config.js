5311e8812456b9d63345ff88752776bc9daea8ac
JC Ivancevich <jc.ivancevich@gmail.com>
2020-06-05 16:47:35 -0300

    use typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
 import pkg from './package.json';
 
 export default [
@@ -12,21 +12,16 @@ export default [
       format: 'umd',
       sourcemap: true,
     },
-    plugins: [resolve(), commonjs()],
+    plugins: [typescript(), resolve()],
   },
-
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
+  // CommonJS (for Node) and ES module (for bundlers) build
   {
     input: 'src/ProtonPlayer.js',
     external: ['bowser'],
     output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' },
+      { format: 'es', file: pkg.module },
+      { format: 'cjs', file: pkg.commonjs },
     ],
+    plugins: [typescript()],
   },
 ];
