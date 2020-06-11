497816028127009a2127a2b325188e0bf1687c8e
JC Ivancevich <jc.ivancevich@gmail.com>
2020-06-11 12:49:15 -0300

    format code

--- src/utils/getFrameLength.js
+++ src/utils/getFrameLength.js
@@ -4,7 +4,7 @@ const bitrateLookup = {
   12: [null, 32, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 256, 320, 384],
   13: [null, 32, 40, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 256, 320],
   21: [null, 32, 48, 56, 64, 80, 96, 112, 128, 144, 160, 176, 192, 224, 256],
-  22: [null, 8, 16, 24, 32, 40, 48, 56, 64, 80, 96, 112, 128, 144, 160]
+  22: [null, 8, 16, 24, 32, 40, 48, 56, 64, 80, 96, 112, 128, 144, 160],
 };
 bitrateLookup[23] = bitrateLookup[22];
 export default function getFrameLength(data, i, metadata) {
@@ -12,8 +12,7 @@ export default function getFrameLength(data, i, metadata) {
   const mpegLayer = metadata.mpegLayer;
   const sampleRate = metadata.sampleRate;
   const bitrateCode = (data[i + 2] & 0b11110000) >> 4;
-  const bitrate =
-    bitrateLookup[`${mpegVersion}${mpegLayer}`][bitrateCode] * 1e3;
+  const bitrate = bitrateLookup[`${mpegVersion}${mpegLayer}`][bitrateCode] * 1e3;
   const padding = (data[2] & 0b00000010) >> 1;
   const length = ~~(mpegLayer === 1
     ? ((12 * bitrate) / sampleRate + padding) * 4
--- src/utils/parseMetadata.js
+++ src/utils/parseMetadata.js
@@ -1,22 +1,22 @@
 const mpegVersionLookup = {
   0: 2,
-  1: 1
+  1: 1,
 };
 const mpegLayerLookup = {
   1: 3,
   2: 2,
-  3: 1
+  3: 1,
 };
 const sampleRateLookup = {
   0: 44100,
   1: 48000,
-  2: 32000
+  2: 32000,
 };
 const channelModeLookup = {
   0: 'stereo',
   1: 'joint stereo',
   2: 'dual channel',
-  3: 'mono'
+  3: 'mono',
 };
 export default function parseMetadata(metadata) {
   const mpegVersion = mpegVersionLookup[metadata.mpegVersion >> 3];
@@ -24,6 +24,6 @@ export default function parseMetadata(metadata) {
     mpegVersion,
     mpegLayer: mpegLayerLookup[metadata.mpegLayer >> 1],
     sampleRate: sampleRateLookup[metadata.sampleRate >> 2] / mpegVersion,
-    channelMode: channelModeLookup[metadata.channelMode >> 6]
+    channelMode: channelModeLookup[metadata.channelMode >> 6],
   };
 }
